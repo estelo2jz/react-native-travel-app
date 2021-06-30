a8c5e165ea371bab1d96eef7f18d9f933970df3f
esteloabell <manitobaaa@gmail.com>
2021-06-30 11:24:23 -0500

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,5 +1,9 @@
 import TextButton from "./TextButton";
+import HeaderBar from "./HeaderBar";
+import TextIconButton from "./TextIconButton";
 
 export {
   TextButton,
+  HeaderBar,
+  TextIconButton,
 }
